73091146ef56f9fb0a79365a9677e3f5103d7995
sadaking <sichirigahama.37@gmail.com>
2020-04-25 17:20:00 +0900

    投稿確認機能の作成

--- app/helpers/connects_helper.rb
+++ app/helpers/connects_helper.rb
@@ -1,2 +1,9 @@
 module ConnectsHelper
+  def choose_new_or_edit
+    if action_name == 'new'|| action_name == 'create'
+      confirm_connects_path
+    elsif action_name == 'edit'
+      connect_path
+    end
+  end
 end
